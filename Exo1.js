d1979ed1ac36631cba03f514dbdc1e9548c28652
yacine <issay584@email.com>
2024-09-25 22:05:09 +0100

    challenge1

---
+++ Exo1.js
@@ -0,0 +1,18 @@
+/*********************Challenge: Exercice jours 3****************************/
+/*-----------------------Exo:1------------------------*/
+let firstName = "yacine"
+let lastName = "le punG"
+let country = "sadoré"
+let city = "Niamey"
+let age = 30
+let isMarried = true
+let year = 2024
+console.log("variable de firstName: " + typeof(firstName) +
+            "\nvariable de lastName: " + typeof(lastName) +
+            "\nvariable de country: " + typeof(country) +
+            "\nvariable de city: " + typeof(city) +
+            "\nvariable de age: " + typeof(age) +
+            "\nvariable de isMarried: " + typeof(isMarried) +
+            "\nvariable de year: " + typeof(year)
+);
+
