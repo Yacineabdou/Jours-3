8d611d2b5d209f0b1db65d23ebd4ed51abb1c6d7
yacine <issay584@email.com>
2024-09-25 23:06:14 +0100

    challenge4

--- Exo1.js
+++ Exo1.js
@@ -1,5 +1,7 @@
 /*********************Challenge: Exercice jours 3****************************/
 /*-----------------------Exo:1------------------------*/
+
+//------------challenge 1
 let firstName = "yacine"
 let lastName = "le punG"
 let country = "sadoré"
@@ -15,4 +17,20 @@ console.log("variable de firstName: " + typeof(firstName) +
             "\nvariable de isMarried: " + typeof(isMarried) +
             "\nvariable de year: " + typeof(year)
 );
+
+//--------------challenge 2
 console.log(parseInt(10));
+
+//---------------challenge 3
+;console.log(parseInt('9.8'));
+
+//--------------challenge 4
+let trueResultat1 = 10 > 5
+let trueResultat2 = 25 === 25
+let trueResultat3 = 15 !== 10
+console.log(trueResultat1, trueResultat2, trueResultat3);
+
+let falseResultat1 = 16 !== 16
+let falseResultat2 = 5 > 10
+let falseResultat3 = 10 === "10"
+console.log(falseResultat1, falseResultat2, falseResultat3);
