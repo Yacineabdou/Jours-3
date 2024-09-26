0e12b530568d398c69d6e18ef4b2d6c84e9ccebf
yacine <issay584@email.com>
2024-09-26 15:48:03 +0100

    challenge 1 et 2

---
+++ Exo2.js
@@ -0,0 +1,13 @@
+/*-------------------Exo:2------------------------------*/
+//---------------challenge 1
+let base = prompt("Entrer base: 20")
+let hauteur = prompt("Entrer height: 10")
+let aire =  0.5 * 20 * 10
+console.log(aire);
+
+//---------------challenge 2
+let coterA = prompt("Entrer side a: 5")
+let coterB = prompt("Entrer side b: 4")
+let coterC = prompt("Entrer c: 3")
+let perimetre = 5 + 4 + 3
+console.log(perimetre);
