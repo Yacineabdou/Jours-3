fc6ecb9b22705c2ba7e77cffda617c3502584e43
yacine <issay584@email.com>
2024-09-27 03:43:45 +0100

    Exo3

--- Exo3.js
+++ Exo3.js
@@ -1,4 +1,11 @@
-const now = new Date()
-const option = { year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit'}
-const formattedDate = now.toLocaleString('fr-FR',option)
+/*-----------------------Exo3--------------------------*/
+const now = new Date();
+
+const year = now.getFullYear();
+const month = (now.getMonth() + 1).toString().padStart(2, '0'); // Ajout de 1 car les mois commencent à 0
+const day = now.getDate().toString().padStart(2, '0');
+const hours = now.getHours().toString().padStart(2, '0');
+const minutes = now.getMinutes().toString().padStart(2, '0');
+
+const formattedDate = `${year}-${month}-${day} ${hours}:${minutes}`;
 console.log(formattedDate);
